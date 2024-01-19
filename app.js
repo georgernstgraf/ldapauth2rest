376bd56eee119eb19299d89bb8ac48c678adafde
Georg Graf (SPG) <grafg@spengergasse.at>
2024-01-19 14:25:15 +0100

    handle broken json

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 console.log('START app.js');
 const express = require('express');
-const cors = require('cors');
 const app = express();
+
+const cors = require('cors');
 app.use(cors());
+
 app.use(express.json());
-const port = Number(process.env.LISTEN_PORT);
+app.use((err, req, res, next) => {
+    if (err.type === 'entity.parse.failed') {
+        res.status(400).json({ error: 'Invalid JSON format' });
+    } else {
+        next(err);
+    }
+});
+
 const { verifyRouter } = require('./routes/verify.js');
 app.use('/verify', verifyRouter);
+
+const port = Number(process.env.LISTEN_PORT);
 app.listen(port, (msg) => {
     if (msg) console.error(msg);
     else console.log(`express app listens on Port ${port}`);
