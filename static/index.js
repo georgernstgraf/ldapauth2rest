9f2594b390276740d36f3e6f96f53845ea239383
Georg Graf (SPG) <grafg@spengergasse.at>
2024-01-19 18:17:21 +0100

    finom

--- static/index.js
+++ static/index.js
@@ -2,31 +2,29 @@ userInput = document.getElementById('username');
 passInput = document.getElementById('password');
 resultOut = document.getElementById('result');
 outbox = document.getElementById('outbox');
-let httpStatus;
 function submit() {
     console.log(`submit: ${userInput.value}`);
     const o = { user: userInput.value, passwd: passInput.value };
-    httpStatus = undefined;
     fetch('', {
         method: 'POST',
         body: JSON.stringify(o),
         headers: { 'Content-Type': 'application/json' },
     })
         .then((res) => {
-            httpStatus = res.status;
-            return res.json();
+            return [res.status, res.json()];
         })
-        .then((data) => {
-            displayResult(data);
+        .then((stat, data) => {
+            displayResult(stat, data);
         })
         .catch((err) => {
-            displayResult(err.message);
+            displayResult(undefined, err.message);
         });
 }
-const colors = ['#aaf0aa', 'lightgrey', '#f0a7a7', 'lightsalmon'];
-async function displayResult(res) {
+const colors = ['#aaf0aa', 'lightgrey', '#f0a7a7', 'lightsalmon', '#9d7149'];
+async function displayResult(stat, res) {
     const str = JSON.stringify(res, null, 2);
     //console.log(str);
-    resultOut.innerHTML = `Status: ${httpStatus}<br>${str}`;
-    outbox.style.backgroundColor = colors[Math.floor(httpStatus / 100) - 2];
+    resultOut.innerHTML = `Status: ${stat}<br>${str}`;
+    stat = stat ? stat : 600;
+    outbox.style.backgroundColor = colors[Math.floor(stat / 100) - 2];
 }
