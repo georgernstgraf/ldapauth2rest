fd0226d2f427b496d3a8289bef175ff80cd70c86
Georg Graf (SPG) <grafg@spengergasse.at>
2024-01-19 17:56:03 +0100

    finom

--- static/index.js
+++ static/index.js
@@ -1,25 +1,32 @@
 userInput = document.getElementById('username');
 passInput = document.getElementById('password');
 resultOut = document.getElementById('result');
-
+outbox = document.getElementById('outbox');
+let httpStatus;
 function submit() {
     console.log(`submit: ${userInput.value}`);
     const o = { user: userInput.value, passwd: passInput.value };
+    httpStatus = undefined;
     fetch('', {
         method: 'POST',
         body: JSON.stringify(o),
         headers: { 'Content-Type': 'application/json' },
     })
         .then((res) => {
-            displayResult(res);
+            httpStatus = res.status;
+            return res.json();
+        })
+        .then((data) => {
+            displayResult(data);
         })
         .catch((err) => {
-            console.log('Georg');
-            console.log(err.message);
+            displayResult(err.message);
         });
 }
+const colors = ['#aaf0aa', 'lightgrey', '#f0a7a7', 'lightsalmon'];
 async function displayResult(res) {
-    const str = JSON.stringify(await res.json(), null, 2);
-    console.log(str);
-    resultOut.innerHTML = `Status: ${res.status}<br>${str}`;
+    const str = JSON.stringify(res, null, 2);
+    //console.log(str);
+    resultOut.innerHTML = `Status: ${httpStatus}<br>${str}`;
+    outbox.style.backgroundColor = colors[Math.floor(httpStatus / 100) - 2];
 }
